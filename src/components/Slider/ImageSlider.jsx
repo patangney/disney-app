6949f2c654879705162ba6048323fcfc7e72f8d6
Patrick Tangney <ptangney@outlook.com>
2022-07-18 20:16:26 +0100

    comming

--- src/components/Slider/ImageSlider.jsx
+++ src/components/Slider/ImageSlider.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Slider from 'react-slick'
+// Import css files
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const ImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  }
   return (
-    <div>ImageSlider</div>
+    <div className='slider'>
+      <Slider {...settings}>
+        <div className='slider__wrapper'>
+          <img src='/assets/images/slider-badging.jpg' alt='slider' />
+        </div>
+      </Slider>
+    </div>
   )
 }
 
