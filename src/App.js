369c442d6fd11222f7a45aa809cc43106b42a0bb
Patrick Tangney <ptangney@outlook.com>
2022-07-18 13:43:17 +0100

    setup scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './components/Header/Header';
+import {Header, Home} from './components/index.js';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='nav'>
+        <img className="nav__logo" src='/assets/images/logo.svg' />   
+        <div className="nav__menu">
+            
+        </div>     
+    </div>
+  )
+}
+
+export default Navigation
