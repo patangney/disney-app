6ed87622d79b9be520bb824ceb7c906630f656a0
Patrick Tangney <ptangney@outlook.com>
2022-07-19 13:59:31 +0100

    update

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Details = () => {
   return (
-    <div>Details</div>
+    <div className="details">
+        <div className="details__container">
+            <div className="details__image-bg">
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/01E873C686EF29975B3760568B754A514BF7D4B5C3E8F89B180C8753EE1D0D78/scale?width=1440&aspectRatio=1.78&format=jpeg" alt=""/>
+            </div>
+        </div>
+    </div>
   )
 }
 
