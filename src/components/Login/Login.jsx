01670f657901ed1e9e2882d90c0283a687787398
Patrick Tangney <ptangney@outlook.com>
2022-07-20 16:11:25 +0100

    added login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className='login__container'>
+        <div className='login__CTA'>
+          <img src='/assets/images/cta-logo-one.svg' alt='cta logo' />
+          <a href='#'>GET ACCESS</a>
+          <p>
+            Host virtual movie nights with GroupWatch. Pause, rewind and react
+            with up to six personal friends. To invite or be invited to join
+            GroupWatch, subscription is required Download any movie or series
+            and watch on-the-go Keep your family safe with easy parental
+            controls An ever-growing range of titles in stunning 4K UHD and
+            Dolby Atmos sound on compatible devices Stream on up to four devices
+            at the same time
+          </p>
+          
+          <img src='/assets/images/cta-logo-two.png' alt='cta logo' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment} from 'react'
 import { Routes, Route } from 'react-router-dom'
-import {Home, Details} from '../components/index.js'
+import {Home, Details, Login} from '../components/index.js'
 
 const AppRouter = () => {
   return (
@@ -8,6 +8,7 @@ const AppRouter = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/details" element={<Details />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     </Fragment>
   )
